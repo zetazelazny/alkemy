0ca6fa57324e496de7b2394aea56e1efb2c4b31d
Zeta <matiaszelazny99@gmail.com>
2022-11-11 12:08:16 -0300

    U11C

---
+++ U11C/doc/diagrama de secuencia.md
@@ -0,0 +1,8 @@
+This is a mermaid diagram, you may need to install a [Browser Plugin](https://github.com/BackMarket/github-mermaid-extension) or [VsCode extension](https://marketplace.visualstudio.com/items?itemName=bierner.markdown-mermaid) or similar to view it.
+
+You can also [view it full screen as an SVG](https://mermaid.ink/svg/c2VxdWVuY2VEaWFncmFtCg==)        
+
+```mermaid
+sequenceDiagram
+
+```
---
+++ U11C/src/calculadora.py
@@ -0,0 +1,36 @@
+def sumar(num1: int | float,
+        num2: int | float) -> int | float | None:
+    try:
+        result = num1 + num2
+    except ValueError:
+        raise (ValueError)
+    return result
+
+
+def restar(num1: int | float,
+        num2: int | float) -> int | float | None:
+    try:
+        result = num1 - num2
+    except ValueError:
+        raise (ValueError)
+    return result
+
+
+def multiplicar(num1: int | float,
+                num2: int | float) -> int | float | None:
+    try:
+        result = num1 * num2
+    except ValueError:
+        raise (ValueError)
+    return result
+
+
+def dividir(num1: int | float,
+            num2: int | float) -> int | float | None:
+    try:
+        result = num1 / num2
+    except ZeroDivisionError:
+        raise (ZeroDivisionError)
+    except ValueError:
+        raise (ValueError)
+    return result
